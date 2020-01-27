e423dad069ee6a3a781446ec3b56a6f8e6ef0bda
Keith James <dev@deoxybyte.co.uk>
2020-01-27 11:51:18 +0000

    ClientPool improvements
    Switch from channels to mutex.
    Added more tests.
    Improved debug message detail.

--- client_pool.go
+++ client_pool.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019. Genome Research Ltd. All rights reserved.
+ * Copyright (C) 2019, 2020. Genome Research Ltd. All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,29 +30,32 @@ import (
 	"github.com/kjsanger/extendo/utilities"
 )
 
-type token struct{}
-type semaphore chan token
-
 // ClientPool is a pool of iRODS Clients for use by applications. It provides
-// a way for an application to obtain a running Client without itself having to
+// a way for an application to obtain a isRunning Client without itself having to
 // manage the number of connections or handle retries when a Client connection
 // can't be obtained for some reason (e.g. the maximum number of connections is
 // reached, a network error occurs, the iRODS server fails).
 //
 // Applications should use the NewClientPool to create a pool and then use
 // the ClientPool's Get() and Return() methods to obtain and release Clients.
+//
+// Once a ClientPool has been created it may be closed. A closed pool will
+// return an error on Get(), but will allow Return(). A closed pool may not
+// be re-opened.
 type ClientPool struct {
-	isOpen     bool          // True if the pool is open
 	clientArgs []string      // baton-do arguments
-	clients    chan *Client  // Running clients in the pool
-	slots      semaphore     // Slots available to create new clients
-	timeout    time.Duration // Timeout for Get calls
-	maxRetries uint8         // Max retries for Get calls
-	sync.Mutex               // Enable locking for Get/Return/Close
+	timeout    time.Duration // Timeout for Get() and Return()
+	maxRetries uint8         // Max retries for Get()
+	sync.Mutex               // Lock for IsOpen(), Get(), Return() and Close()
+	isOpen     bool          // True if the pool is open
+	clients    []*Client     // Running clients in the pool
+	numClients uint8
+	maxClients uint8
 }
 
 var (
 	errPoolClosed = errors.New("the client pool is closed")
+	errPoolEmpty  = errors.New("the client pool is empty")
 	errDeadClient = errors.New("dead client in the client pool")
 	errGetTimeout = errors.New("timeout getting client from the pool")
 )
@@ -68,12 +71,11 @@ func NewClientPool(maxSize uint8, timeout time.Duration,
 	processedArgs = utilities.Uniq(append(processedArgs, clientArgs...))
 
 	pool := ClientPool{
-		isOpen:     true,
 		clientArgs: processedArgs,
 		timeout:    timeout,
 		maxRetries: uint8(3),
-		slots:      make(semaphore, maxSize),
-		clients:    make(chan *Client, maxSize),
+		isOpen:     true,
+		maxClients: maxSize,
 	}
 
 	return &pool
@@ -81,20 +83,16 @@ func NewClientPool(maxSize uint8, timeout time.Duration,
 
 // IsOpen returns true if the pool is open.
 func (pool *ClientPool) IsOpen() bool {
-	return pool.isOpen
-}
-
-// Get returns a running Client from the pool, or creates a new one. It returns
-// an error if the pool is closed, if the attempt to get a Client exceeds the
-// pool's timeout, or if an error is encountered creating the Client.
-func (pool *ClientPool) Get() (*Client, error) {
 	pool.Lock()
 	defer pool.Unlock()
 
-	if !pool.IsOpen() {
-		return nil, errPoolClosed
-	}
+	return pool.isOpen
+}
 
+// Get returns a isRunning Client from the pool, or creates a new one. It returns
+// an error if the pool is closed, if the attempt to get a Client exceeds the
+// pool's timeout, or if an error is encountered creating the Client.
+func (pool *ClientPool) Get() (*Client, error) {
 	return pool.getWithRetries()
 }
 
@@ -129,94 +127,131 @@ func (pool *ClientPool) getWithTimeout() (*Client, error) {
 	timeout := time.NewTimer(pool.timeout)
 	defer timeout.Stop()
 
-	select {
-	case client := <-pool.clients:
-		log.Debug().Msgf("got a client from the pool leaving %d",
-			len(pool.clients))
-		return client, nil
-
-	case <-timeout.C:
-		timeout.Reset(pool.timeout)
-
-		log.Debug().Msg("timed out getting a client from the pool")
-
+	for {
 		select {
-		case client := <-pool.clients:
-			log.Debug().Msg("got client from the pool while waiting " +
-				"to add a new one")
-			return client, nil
-
-		case pool.slots <- token{}:
-			log.Debug().Msgf("added new client to the pool making %d",
-				len(pool.clients))
-			client, err := FindAndStart(pool.clientArgs...)
-			if err != nil {
-				<-pool.slots // Free the slot on error
-			}
-			return client, err
-
 		case <-timeout.C:
 			return nil, errGetTimeout
+		default:
+			pool.Lock()
+			if !pool.isOpen {
+				pool.Unlock()
+				return nil, errPoolClosed
+			}
+
+			if !pool.isEmpty() {
+				client, err := pool.pop()
+				log.Debug().Msgf("got a client from the pool leaving %d",
+					pool.size())
+
+				pool.Unlock()
+				return client, err
+			}
+
+			if pool.numClients < pool.maxClients {
+				client, err := FindAndStart(pool.clientArgs...)
+				if err == nil {
+					pool.numClients++
+					log.Debug().Msgf("added new client to the pool making %d",
+						pool.numClients)
+				}
+
+				pool.Unlock()
+				return client, err
+			}
+
+			pool.Unlock()
+
+			interval := time.Microsecond * 100
+			log.Debug().Msgf("sleeping ... %s", interval)
+			time.Sleep(interval)
 		}
 	}
 }
 
-// Return allows a client to be returned to the pool. If the client is running,
+// Return allows a client to be returned to the pool. If the client is isRunning,
 // it is returned to the pool. If the client has crashed or been stopped, this
 // method will discard it and decrement the client count so that a new one can
 // be created. If the pool has been closed, clients may still be returned,
 // where they will be stopped and any errors from this ignored.
 func (pool *ClientPool) Return(client *Client) error {
+	log := logs.GetLogger()
+
 	pool.Lock()
 	defer pool.Unlock()
 
-	log := logs.GetLogger()
-	if !pool.IsOpen() {
-		log.Debug().Msg("returned 1 client to a closed pool")
+	if !pool.isOpen {
+		log.Debug().Msg("discarding 1 client returned to a closed pool")
 		client.StopIgnoreError()
 		return nil
 	}
 
 	if !client.IsRunning() {
-		log.Debug().Msg("discarded 1 client (stopped)")
-		<-pool.slots
+		log.Debug().Msg("discarding 1 client (stopped)")
+		pool.numClients--
 		return nil
 	}
 
-	select {
-	case pool.clients <- client:
-		log.Debug().Msgf("returned 1 client (running) to the pool making %d",
-			len(pool.clients))
+	if uint8(pool.size()) < pool.maxClients {
+		pool.push(client)
+		log.Debug().Msgf("returned 1 client (isRunning) to the pool making %d",
+			pool.size())
 		return nil
-
-	default:
-		log.Debug().Msg("discarded 1 client (running), pool full")
-		<-pool.slots
-		return client.Stop()
 	}
+
+	log.Debug().Msg("discarding 1 client (isRunning), pool full")
+
+	return client.Stop()
 }
 
 // Close closes the pool for further Get() operations. Clients may still be
-// returned to a closed pool, see Return.
+// returned to a closed pool, see Return().
 func (pool *ClientPool) Close() {
 	pool.Lock()
 	defer pool.Unlock()
 
-	if !pool.IsOpen() {
+	if !pool.isOpen {
 		return
 	}
-
 	pool.isOpen = false
 
 	log := logs.GetLogger()
-	log.Debug().Msg("closing client channel")
-	close(pool.clients)
+	log.Debug().Msgf("stopping %d clients", pool.size())
 
-	for c := range pool.clients {
-		log.Debug().Msg("stopping client")
-		err := c.Stop()
+	for pool.size() > 0 {
+		c, err := pool.pop()
 		if err != nil {
-			log.Error().Err(err).Msg("client did not stop cleanly")
+			log.Error().Err(err).Msg("internal error")
+		}
+
+		log.Debug().Int("pid", c.ClientPid()).Msg("stopping client")
+		err = c.Stop()
+		if err != nil {
+			log.Error().Err(err).
+				Int("pid", c.ClientPid()).
+				Msg("client did not stop cleanly")
 		}
 	}
+}
+
+func (pool *ClientPool) size() int {
+	return len(pool.clients)
+}
+
+func (pool *ClientPool) isEmpty() bool {
+	return pool.size() == 0
+}
+
+func (pool *ClientPool) pop() (*Client, error) {
+	if pool.isEmpty() {
+		return nil, errPoolEmpty
+	}
+
+	n := pool.size()
+	stack, top := pool.clients[:n-1], pool.clients[n-1]
+	pool.clients = stack
+	return top, nil
+}
+
+func (pool *ClientPool) push(client *Client) {
+	pool.clients = append(pool.clients, client)
 }
